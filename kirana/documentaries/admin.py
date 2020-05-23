4f7c1b9e6e1c70b95e3ff49ebf23c236ab26852b
unrecano <togapaulo24@gmail.com>
2020-05-23 00:41:06 -0500

    editar administrador.

--- kirana/documentaries/admin.py
+++ kirana/documentaries/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Documentary, Tag
 
-admin.site.register(Documentary)
-admin.site.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ["value"]
+    ordering = ["value"]
+
+class DocumentaryAdmin(admin.ModelAdmin):
+    list_display = ["title", "url", "year", "duration"]
+    search_fields = ["title", "description"]
+    list_filter = ["year"]
+    ordering = ['created']
+
+admin.site.register(Documentary, DocumentaryAdmin)
+admin.site.register(Tag, TagAdmin)
