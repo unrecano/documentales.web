b2b4cd4a6b89788a5ad9d7cc252dff9c733dbe99
unrecano <togapaulo24@gmail.com>
2020-05-23 00:43:03 -0500

    crear comando para llenado de base de datos.

--- kirana/documentaries/management/commands/load_documentaries.py
+++ kirana/documentaries/management/commands/load_documentaries.py
@@ -5,43 +5,65 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils.text import slugify
 from documentaries.models import Documentary, Tag
 
-# # Database credentials.
-# DB_USER = os.getenv('SCRAPING_DB_USER')
-# DB_PASS = os.getenv('SCRAPING_DB_PASS')
-# DB_HOST = os.getenv('SCRAPING_DB_HOST')
+# Database credentials.
+DB_USER = os.getenv('SCRAPING_DB_USER')
+DB_PASS = os.getenv('SCRAPING_DB_PASS')
+DB_HOST = os.getenv('SCRAPING_DB_HOST')
 
-# # Conectar a base de datos y obtener colecciones.
-# uri = f"mongodb+srv://{DB_USER}:{DB_PASS}@{DB_HOST}/"
-# client = pymongo.MongoClient(f"{uri}?retryWrites=true&w=majority")
-# db = client.scraping
-# documentaries_collection = db.documentaries
+# Conectar a base de datos y obtener colecciones.
+uri = f"mongodb+srv://{DB_USER}:{DB_PASS}@{DB_HOST}/"
+client = pymongo.MongoClient(f"{uri}?retryWrites=true&w=majority")
+db = client.scraping
+documentaries_collection = db.documentaries
 
 class Command(BaseCommand):
+
+    def _get_or_created_tags(self, tags):
+        array = []
+        for element in tags:
+            value = slugify(element.lower())
+            tag = Tag.objects.filter(value=value).first()
+            if tag:
+                self.stdout.write(self.style.WARNING(f"{tag.value} (existe)"))
+                array.append(tag)
+            else:
+                new = Tag.objects.create(value=value)
+                self.stdout.write(self.style.SUCCESS(f"{new.value} (creada)"))
+                array.append(new)
+        return array
+
     def handle(self, *args, **options):
-        data = [
-            {
-                '_id': '5ec89a159fbf3dbfff70c4f5',
-                'url': 'https://www.documentarymania.com/player.php?title=The+Incredible+Human+Journey%3A+Asia',
-                'year': '2009',
-                'duration': 59,
-                'title': 'The Incredible Human Journey: Asia',
-                'description': "The journey continues into Asia, the world's greatest land mass, in a quest to discover how early hunter-gatherers managed to survive in one of the most inhospitable places on earth - the Arctic region of Northern Siberia. Alice also explores what may have occurred during human migration to produce Chinese physical characteristics, and considers a controversial claim about Chinese evolution: that the Chinese do not share the same African ancestry as other peoples.",
-                'tags': ['History', 'Prehistory', 'Evolution', 'Genetics', 'Homo sapiens', 'Siberia China', 'Asia', 'The Incredible Human Journey'],
-                'site': 'https://www.documentarymania.com',
-                'inserted_at': datetime.datetime(2020, 5, 22, 22, 35, 49, 401000)
-            },
-            {
-                '_id': '5ec89a169fbf3dbfff70c4f6',
-                'url': 'https://www.documentarymania.com/player.php?title=The+Incredible+Human+Journey%3A+Australia',
-                'year': '2009',
-                'duration': 59,
-                'title': 'The Incredible Human Journey: Australia',
-                'description': "Dr Alice Roberts looks at our ancestors' seemingly impossible journey to Australia. Miraculously preserved footprints and very old human fossils buried in the outback suggest a mystery: that humans reached Australia almost before anywhere else. How could they have travelled so far from Africa, crossing the open sea on the way, and do it thousands of years before they made it to Europe? The evidence trail is faint and difficult to pick up, but Alice takes on the challenge. In India, new discoveries among the debris of a super volcano hint that our species started the journey much earlier than previously thought, while in Malaysia, genetics points to an ancient trail still detectable in the DNA of tribes today.\r\nAlice travels deep into the Asian rainforests in search of the first cavemen of Borneo and tests out a Stone Age raft to see whether sea travel would have been possible thousands of years ago, before coming to a powerful conclusion.",
-                'tags': ['History', 'Prehistory', 'Evolution', 'Genetics', 'Homo sapiens', 'Australia', 'The Incredible Human Journey'],
-                'site': 'https://www.documentarymania.com',
-                'inserted_at': datetime.datetime(2020, 5, 22, 22, 35, 50, 888000)
-            }
-        ]
-        self.stdout.write(self.style.SUCCESS("Cargando ..."))
-        for documentary in data:
-            print(documentary)
+        for d in documentaries_collection.find({}):
+            # Verificar si el documental existe.
+            # Si no existe se crea.
+            slug = slugify(d.get('title'))
+            documentary = Documentary.objects.filter(slug=slug).first()
+            if documentary:
+                self.stdout.write(self.style.WARNING(f"{documentary.title} (existe)"))
+                # Si contiene un nuevo sitio se agrega al campo sites y
+                # se agregan las nuevas etiquetas.
+                if not d.get('site') in documentary.sites:
+                    sites = documentary.sites
+                    sites.append(d.get('site'))
+                    documentary.sites = sites
+                    # Obtener tags y agregarlas a la instancia Documentary.
+                    tags = self._get_or_created_tags(d.get('tags'))
+                    documentary.tags.set(tags)
+                    documentary.save()
+                    self.stdout.write(self.style.SUCCESS(f"{documentary.title} (actualizado)"))
+            else:
+                # Crear nuevos tags.
+                tags = self._get_or_created_tags(d.get('tags'))
+                # Crear nuevo documental.
+                params = {
+                    "title": d.get('title'),
+                    "slug": slug,
+                    "description": d.get('description'),
+                    "year": d.get('year') if d.get('year') else None,
+                    "duration": d.get('duration') if d.get('duration') else None,
+                    "url": d.get('url'),
+                    "sites": [d.get('site')],
+                }
+                created = Documentary.objects.create(**params)
+                created.tags.set(tags)
+                self.stdout.write(self.style.SUCCESS(f"{created.title} (creado)"))
