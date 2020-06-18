e8a39f2a6a0e67c787e0b5d247fade4533601ff0
Paulo Vicente Torres Gaviño <paulo@lacafetalab.pe>
2020-06-18 15:28:24 -0500

    agregar test resolve.

--- kirana/documentaries/tests.py
+++ kirana/documentaries/tests.py
@@ -27,3 +27,22 @@ class RedirectFromHomePageTestCase(TestCase):
         visitors = self.url.visitors
         self.url.add_visitor()
         self.assertEqual(self.url.visitors, visitors + 1)
+
+class DetailDocumentaryTestCase(TestCase):
+
+    fixtures = ['tags', 'documentaries', 'urls']
+
+    def test_detail_resolves(self):
+        documentary = Documentary.objects.first()
+        view = resolve(reverse("detail", args=[documentary.slug]))
+        self.assertEqual(view.func.__name__, views.detail.__name__)
+
+class AboutPageTestCase(TestCase):
+    def test_about_resolves(self):
+        view = resolve("/about")
+        self.assertEqual(view.func.__name__, views.about.__name__)
+
+class PointDocumentaryTestCase(TestCase):
+    def test_point_resolves(self):
+        view = resolve("/point")
+        self.assertEqual(view.func.__name__, views.point.__name__)
--- kirana/documentaries/urls.py
+++ kirana/documentaries/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('documentaries/<slug:slug>/', views.detail, name="detail"),
     path('about', views.about, name="about"),
     path('urls/<uuid:id>/', views.redirect_url, name="redirect"),
-    path('point/', views.point, name="point")
+    path('point', views.point, name="point")
 ]
