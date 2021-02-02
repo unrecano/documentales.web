2732bdfb2e6637b4eac7ae89eb85515cc8f882ec
unrecano <unrecano@gmail.com>
2021-02-02 17:16:20 -0500

    leer secret key desde variable de entorno.

--- kirana/kirana/settings.py
+++ kirana/kirana/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e!g%dge-s5@d+v5n2&547u%yqpuzc%x@=e^e1pa#0b1uupa9am'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
