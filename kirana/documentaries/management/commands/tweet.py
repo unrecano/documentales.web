d8b04bb4b08bcadad1b21597fd9ffdd9be102357
unrecano <unrecano@gmail.com>
2021-02-03 00:09:39 -0500

    twittear documentales.

--- kirana/documentaries/management/commands/tweet.py
+++ kirana/documentaries/management/commands/tweet.py
@@ -1,6 +1,39 @@
+import os
+import tweepy
 from django.core.management.base import BaseCommand, CommandError
 from documentaries.models import Documentary
 
 class Command(BaseCommand):
+    
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('tweet'))
+        twitter_auth_keys = {
+            "consumer_key": os.getenv("CONSUMER_KEY"),
+            "consumer_secret": os.getenv("CONSUMER_SECRET"),
+            "access_token": os.getenv("ACCESS_TOKEN"),
+            "access_token_secret": os.getenv("ACCESS_TOKEN_SECRET")
+        }
+        auth = tweepy.OAuthHandler(
+            twitter_auth_keys['consumer_key'],
+            twitter_auth_keys['consumer_secret']
+        )
+        auth.set_access_token(
+            twitter_auth_keys['access_token'],
+            twitter_auth_keys['access_token_secret']
+        )
+        api = tweepy.API(auth)
+
+        documentaries = Documentary.objects.filter(tweeted=False).all()[:2]
+        for documentary in documentaries:
+            urls = "\n".join([s.url for s in documentary.sites.all()])
+            tags = [f"#{t}" for t in documentary.tags]
+            hashtags = " ".join(tags)
+            tweet = f"""Have you see this documentary: '{documentary.title}'
+see it: {urls}
+{hashtags} #kirana #documentaries"""
+            self.stdout.write(self.style.SUCCESS(tweet[:280]))
+            try:
+                api.update_status(tweet[:280])
+                documentary.tweeted = True
+                documentary.save()
+            except tweepy.TweepError as error:
+                print(str(error))
