14a3401fbdf40e3cf81fa2e75b5b87af1a5b6332
unrecano <unrecano@gmail.com>
2021-03-07 23:38:10 -0500

    add user-agent into request.

--- documentaries/web/crawlers/utils.py
+++ documentaries/web/crawlers/utils.py
@@ -1,6 +1,8 @@
 """
 Utils methods for crawlers.
 """
+import random
+from urllib import request
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,5 +12,12 @@ def get_html_from_url(url):
 
     url -- string.
     """
-    page = requests.get(url)
+    headers = {"user-agent": get_user_agent()}
+    page = requests.get(url, headers=headers)
     return BeautifulSoup(page.content, 'lxml')
+
+def get_user_agent():
+    url = "https://gitlab.com/-/snippets/2087548/raw/master/snippetfile1.txt"
+    content = requests.get(url).content.decode("utf-8")
+    ua = content.split("\n")[:-1]
+    return random.choice(ua)
