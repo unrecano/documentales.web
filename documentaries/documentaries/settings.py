a54d6f0074484aab96501ad41cec0bcceac54d60
unrecano <unrecano@gmail.com>
2021-03-21 23:07:59 -0500

    add pagination for documentaries.

--- documentaries/documentaries/settings.py
+++ documentaries/documentaries/settings.py
@@ -168,6 +168,14 @@ LOGGING = {
 }
 
 
+# Config Django rest framework.
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25
+}
+
+
 # Config for debug toolbar.
 
 if DEBUG:
